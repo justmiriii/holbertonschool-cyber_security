2d1dfdb31ccb423479347b86ebb5e228e720029c
justmiriii <11880@holbertonstudents.com>
2025-08-16 04:32:37 -0400

    new

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/^Registrant/ || /^Admin/ || /^Tech/' > $1.csv
+whois $1 | awk -F': ' '/(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext|Fax|Fax Ext|Email)/ {gsub(/^[ \t]+|[ \t]+$/, "", $1); if (NF==2) {sub(/^[ \t]+/, "", $2); print $1 "," $2} else {print $1 ","} }' > $1.csv

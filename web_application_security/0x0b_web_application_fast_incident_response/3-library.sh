7b9123168ac203094345407b1f27cc75cfb2d2ad
justmiriii <11880@holbertonstudents.com>
2026-06-23 17:55:25 +0400

    Update 3-library.sh

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 attacker_ip=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
-grep "^$attacker_ip " logs.txt | awk -F'"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | sed 's/^[[:space:]]*[0-9]*[[:space:]]*//'
+grep "^$attacker_ip " logs.txt | awk -F'"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; sub(/^ /,""); print}'

0de51e89cc3067dbcf5685eccedce2d7a64fccd0
justmiriii <11880@holbertonstudents.com>
2026-06-23 17:50:00 +0400

    Update 3-library.sh

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}' | xargs -I{} awk -v ip="{}" '$1 == ip {print $0}' logs.txt | awk -F'"' '{print $6}' | sort | uniq
+ATTACKER_IP=$(awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
+grep "^$ATTACKER_IP" logs.txt | awk -F\" '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
